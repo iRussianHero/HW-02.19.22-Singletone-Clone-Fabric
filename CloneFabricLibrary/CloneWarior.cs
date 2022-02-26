7bfde670254de49a95cbe30f62e3e593036c9bea
Yakushev Roman <boxboxbox@inbox.ru>
2022-02-26 08:58:49 +0300

    Add class CloneWarrior

---
+++ CloneFabricLibrary/CloneWarior.cs
@@ -0,0 +1,22 @@
+﻿namespace CloneFabricLibrary
+{
+    public class CloneWarior
+    {
+        public string Name { get; set; }
+        public string ClassType { get; set; }
+
+        public CloneWarior(string name, string classType)
+        {
+            Name = name;
+            ClassType = classType;
+        }
+        public CloneWarior Clone()
+        {
+            return new CloneWarior(Name, ClassType);
+        }
+        public string GetInfo()
+        {
+            return ($"Name: {Name}, Class - {ClassType}");
+        }
+    }
+}
